b5f512dd6ad8e6c387f4b08b2b335ad0bc5c78ed
rafaelfarran <rafaelandradedev@gmail.com$ git config --global user.email rafaelandradedev@gmail.com>
2022-02-18 21:11:14 -0300

    add: get quizz from API

--- script/script.js
+++ script/script.js
@@ -3,11 +3,22 @@ let quizzes = [];
 const promisse = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
 promisse.then(promessaCumprida);
 
-function promessaCumprida(resposta){
+function promessaCumprida(resposta) {
     console.log(resposta)
     quizzes = resposta.data;
+    renderizarQuizzesNaTela()
 }
 
-function renderizarQuizzesNaTela(indice){
-    const quizzesServer = document.querySelector(".quizzes-from-server");
+function renderizarQuizzesNaTela() {
+    const quizzesServer = document.querySelector(".quizz-list");
+        quizzesServer.innerHTML = "";
+    for (let i = 0; i < 6; i++) {
+        quizzesServer.innerHTML += `
+    <div class="quizzes-from-server">
+    <img src="${quizzes[i].image}">
+    <div class="titulo-quizz">
+    <P>${quizzes[i].title}</P>
+    </div>
+    `;
+    }
 }
